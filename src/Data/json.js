f8f366eb7beb7aa830b8bc497bcd77daab621c5f
Todd Jones <jonesgtodd@gmail.com>
2021-01-02 17:01:41 -0600

    added header css and js

---
+++ src/Data/json.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        userId: 1,
+        image: "",
+        name: { first: "Tom", last: "Wilson" },
+        phone: "2155555555",
+        email: "tomwilson;learningcontainer.com",
+        dob: "08-13-1987"
+    },
+    {
+        userId: 2,
+        image: "",
+        name: { first: "Todd", last: "Morris" },
+        phone: "123456",
+        email: "todmorris;learningcontainer.com",
+        dob: "05-15-1980"
+    },
+    {
+        userId: 3,
+        image: "",
+        name: { first: "Sandy", last: "Jones" },
+        phone: "2354430",
+        email: "sandyjones@learningcontainer.com",
+        dob: "002-02-2001"
+    }
+]
+
+export default persons;
