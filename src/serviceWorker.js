1f2d4db61edd0a1c19fc7ad409197a2d851b4eb5
Todd Jones <jonesgtodd@gmail.com>
2021-01-02 16:38:56 -0600

    added servicesworker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -24,8 +24,8 @@ const isLocalhost = Boolean(
       
           navigator.serviceWorker.ready.then(() => {
             console.log(
-              'This web app is being served cache-first by a service ' +
-                'worker. To learn more, visit https://bit.ly/CRA-PWA'
+              ' ' +
+                ''
             );
           });
         } else {
@@ -35,4 +35,85 @@ const isLocalhost = Boolean(
       });
     }
   }
+
+  function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker == null) {
+            return;
+          }
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                
+                console.log(
+                  ' ' +
+                    '.'
+                );
   
+                // Execute callback
+                if (config && config.onUpdate) {
+                  config.onUpdate(registration);
+                }
+              } else {
+                
+                console.log('Content is cached for offline use.');
+  
+                // Execute callback
+                if (config && config.onSuccess) {
+                  config.onSuccess(registration);
+                }
+              }
+            }
+          };
+        };
+      })
+      .catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
+  }
+  
+  function checkValidServiceWorker(swUrl, config) {
+    
+    fetch(swUrl, {
+      headers: { 'Service-Worker': 'script' }
+    })
+      .then(response => {
+       
+        const contentType = response.headers.get('content-type');
+        if (
+          response.status === 404 ||
+          (contentType != null && contentType.indexOf('javascript') === -1)
+        ) {
+        
+          navigator.serviceWorker.ready.then(registration => {
+            registration.unregister().then(() => {
+              window.location.reload();
+            });
+          });
+        } else {
+          // Service worker found. Proceed as normal.
+          registerValidSW(swUrl, config);
+        }
+      })
+      .catch(() => {
+        console.log(
+          'No internet connection found. App is running in offline mode.'
+        );
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then(registration => {
+          registration.unregister();
+        })
+        .catch(error => {
+          console.error(error.message);
+        });
+    }
+  }
